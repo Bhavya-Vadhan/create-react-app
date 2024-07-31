051421c143c99008780fdeb2896aaaa888c42a0c
Bhavya <bhavyavadhan4@gmail.com>
2024-07-31 20:18:48 +0530

    testing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Components/Home"
+import About from "./Components/About"
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
